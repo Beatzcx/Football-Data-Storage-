33abce271cff2637f714683c6599457830fce407
Charles Stover <26stoverc@mydacc.org>
2026-01-06 12:35:46 -0500

    wow

---
+++ crud.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>CRUD APPLICATION IN PHP</h1>
+    <div class ="table table-hover table-bordered table-striped">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>FIRST NAME</th>
+                    <th>LAST NAME</th>
+                    <th>AGE</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                 <td>3</td>
+                 <td>John</td>
+                    <td>Doe</td>
+                    <td>25</td>
+                </tr>
+                <tr>
+                 <td>4</td>
+                 <td>Jane</td>
+                    <td>Smith</td>
+                    <td>30</td>
+                </tr>
+            </tbody>
+            <tr>
+                <td>5</td>
+                <td>Emily</td>
+                <td>Jones</td>
+                <td>22</td>
+
+        </table>
+    </div>
+</body>
